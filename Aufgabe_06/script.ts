f82f1d2d0f86bad608434781f68380b8f33fe54a
MarieFelizia <115719520+MarieFelizia@users.noreply.github.com>
2022-11-18 19:07:41 +0100

    Dateien hinzugefügt

---
+++ Aufgabe_06/script.ts
@@ -0,0 +1,15 @@
+let EuPopulation2022 :number = 446.83;
+let DePopulation2022 :number = 83.24;
+let ItPopulation2022 :number = 58.98;
+let FrPopulation2022 :number = 67.43;
+let SpPopulation2022 :number = 47.43; 
+
+const EuPopulation2008 :number = 438.73;
+const DePopulation2008 :number = 82.11;
+const ItPopulation2008 :number = 58.83;
+const FrPopulation2008 :number = 64.37;
+const SpPopulation2008 :number = 45.95; 
+
+
+console.log(EuPopulation2022 + " Mio")
+console.log()
