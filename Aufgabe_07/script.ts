704b9366849e0da48aa930e053429133f311fac2
MarieFelizia <115719520+MarieFelizia@users.noreply.github.com>
2022-11-25 10:49:39 +0100

    änderung

--- Aufgabe_07/script.ts
+++ Aufgabe_07/script.ts
@@ -18,10 +18,10 @@ console.log("Im Jahr 2022 leben insgesamt " + DePopulation2022 + " Mio Einwohner
 let ErgebnisDe: string = parseFloat (DePopulation2022 / EuPopulation2022 * 100 + " %").toFixed (0); 
 console.log("Das sind " + ErgebnisDe + " % relativ zur Gesamtzahl in der EU");
 
-let ErgebnisDe2= ((DePopulation2022 - DePopulation2008) / DePopulation2008) * 100;
+let ErgebnisDe2: string = parseFloat ((DePopulation2022 - DePopulation2008) / DePopulation2008 * 100 + " %").toFixed (1);
 console.log("Die Wachstumsrate in Deutschland seit 2008 liegt bei " + ErgebnisDe2 + " %");
 
-let ErgebnisDe3= DePopulation2022 - DePopulation2008; 
+let ErgebnisDe3: string = (DePopulation2022 - DePopulation2008).toFixed (2); 
 console.log("Die gesamte Wachstumsrate seit 2008 bis 2022 liegt bei " + ErgebnisDe3 + " Mio");
 
 
@@ -30,13 +30,13 @@ console.log("Die gesamte Wachstumsrate seit 2008 bis 2022 liegt bei " + Ergebnis
 
 console.log("Im Jahr 2022 leben insgesamt " + ItPopulation2022 + " Mio EinwohnerInnen in Italien");
 
-let ErgebnisIt= (ItPopulation2022 / EuPopulation2022) * 100; 
+let ErgebnisIt: string = parseFloat ((ItPopulation2022 / EuPopulation2022) * 100 + " %").toFixed (0); 
 console.log("Das sind " + ErgebnisIt + " % relativ zur Gesamtzahl in der EU");
 
-let ErgebnisIt2= ((ItPopulation2022 - ItPopulation2008) / ItPopulation2008) * 100;
+let ErgebnisIt2: string = parseFloat ((ItPopulation2022 - ItPopulation2008) / ItPopulation2008 * 100 + " %").toFixed (1);
 console.log("Die Wachstumsrate in Italien seit 2008 liegt bei " + ErgebnisIt2 + " %");
 
-let ErgebnisIt3= ItPopulation2022 - ItPopulation2008; 
+let ErgebnisIt3: string = parseFloat (ItPopulation2022 - ItPopulation2008 + " Mio").toFixed (2); 
 console.log("Die gesamte Wachstumsrate seit 2008 bis 2022 liegt bei " + ErgebnisIt3 + " Mio");
 
 
@@ -45,13 +45,13 @@ console.log("Die gesamte Wachstumsrate seit 2008 bis 2022 liegt bei " + Ergebnis
 
 console.log("Im Jahr 2022 leben insgesamt " + FrPopulation2022 + " Mio EinwohnerInnen in Frankreich");
 
-let ErgebnisFr= (FrPopulation2022 / EuPopulation2022) * 100; 
+let ErgebnisFr: string = parseFloat ((FrPopulation2022 / EuPopulation2022) * 100 + " %").toFixed (0); 
 console.log("Das sind " + ErgebnisFr + " % relativ zur Gesamtzahl in der EU");
 
-let ErgebnisFr2= ((FrPopulation2022 - FrPopulation2008) / FrPopulation2008) * 100;
+let ErgebnisFr2: string = parseFloat ((FrPopulation2022 - FrPopulation2008) / FrPopulation2008 * 100 + " %").toFixed (1);
 console.log("Die Wachstumsrate in Frankreich seit 2008 liegt bei " + ErgebnisFr2 + " %");
 
-let ErgebnisFr3= FrPopulation2022 - FrPopulation2008; 
+let ErgebnisFr3: string = parseFloat (FrPopulation2022 - FrPopulation2008 + " Mio").toFixed (2); 
 console.log("Die gesamte Wachstumsrate seit 2008 bis 2022 liegt bei " + ErgebnisFr3 + " Mio");
 
 
@@ -60,13 +60,13 @@ console.log("Die gesamte Wachstumsrate seit 2008 bis 2022 liegt bei " + Ergebnis
 
 console.log("Im Jahr 2022 leben insgesamt " + SpPopulation2022 + " Mio EinwohnerInnen in Spanien");
 
-let ErgebnisSp= (SpPopulation2022 / EuPopulation2022) * 100; 
+let ErgebnisSp: string = parseFloat ((SpPopulation2022 / EuPopulation2022) * 100 + " %").toFixed (0); 
 console.log("Das sind " + ErgebnisSp + " % relativ zur Gesamtzahl in der EU");
 
-let ErgebnisSp2= ((SpPopulation2022 - SpPopulation2008) / SpPopulation2008) * 100;
+let ErgebnisSp2: string = parseFloat ((SpPopulation2022 - SpPopulation2008) / SpPopulation2008 * 100 + " %").toFixed (1);
 console.log("Die Wachstumsrate in Spanien seit 2008 liegt bei " + ErgebnisSp2 + " %");
 
-let ErgebnisSp3= SpPopulation2022 - SpPopulation2008; 
+let ErgebnisSp3: string = parseFloat (SpPopulation2022 - SpPopulation2008 + "Mio").toFixed (2); 
 console.log("Die gesamte Wachstumsrate seit 2008 bis 2022 liegt bei " + ErgebnisSp3 + " Mio");
 
 
