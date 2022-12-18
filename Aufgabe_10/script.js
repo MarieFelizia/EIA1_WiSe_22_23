52ab9230dbff85d2431b2a5ff88c10fc54aa0ee4
MarieFelizia <115719520+MarieFelizia@users.noreply.github.com>
2022-12-18 16:48:57 +0100

    Änderung final

--- Aufgabe_10/script.js
+++ Aufgabe_10/script.js
@@ -1,17 +1,60 @@
-let Enter = document.getElementById("inputText");
-let taskNumber = 0;
-Enter.addEventListener("keypress", function (event) {
-    if (event.key == "enter") {
-        addTask();
-        Enter.value = " ";
+window.addEventListener("load", function () {
+    let enter = document.getElementById("inputText");
+    let tasksNumber = 0;
+    enter.addEventListener("keypress", function (event) {
+        if (event.key == "Enter") {
+            addTask();
+            enter.value = " ";
+        }
+    });
+    function addTask() {
+        const newDiv = document.createElement("div");
+        newDiv.className = "myClass";
+        const Kreis = document.createElement("img");
+        Kreis.className = "checkBox";
+        Kreis.setAttribute("src", "u5.svg");
+        Kreis.setAttribute("width", "40");
+        Kreis.setAttribute("height", "40");
+        const Text = document.createElement("label");
+        Text.innerHTML = enter.value;
+        Text.className = "taskText";
+        Text.setAttribute("width", "600");
+        Text.setAttribute("height", "40");
+        const Haken = document.createElement("img");
+        Haken.className = "Haken";
+        Haken.setAttribute("src", "u7.svg");
+        Haken.setAttribute("width", "20");
+        const trash = document.createElement("img");
+        trash.setAttribute("src", "u8.svg");
+        trash.className = "trash";
+        trash.setAttribute("width", "30");
+        trash.setAttribute("height", "30");
+        document.getElementById("toDoList").appendChild(newDiv);
+        newDiv.appendChild(Kreis);
+        newDiv.appendChild(Text);
+        newDiv.appendChild(trash);
+        Kreis.addEventListener("click", function () {
+            newDiv.appendChild(Haken);
+        });
+        Haken.addEventListener("click", function () {
+            newDiv.removeChild(Haken);
+        });
+        function deleteTask(div) {
+            newDiv.remove();
+            tasksNumber--;
+            tasksAnzahl();
+        }
+        ;
+        trash.addEventListener("click", function (event) {
+            console.log(event);
+            deleteTask(newDiv);
+        });
+        tasksNumber++;
+        tasksAnzahl();
     }
+    function tasksAnzahl() {
+        document.getElementById("counter").innerText = tasksNumber + " insgesamt";
+    }
+    ;
 });
-function addTask() {
-}
-const wrapper = document.querySelector(".wrapper");
-const newDiv = document.createElement("div");
-newDiv.className = "myClass";
-newDiv.id = "myId";
-newDiv.innerHTML = "essen";
-wrapper.appendChild(newDiv);
 //# sourceMappingURL=script.js.map
--- Aufgabe_10/script.ts
+++ Aufgabe_10/script.ts
@@ -1,22 +1,78 @@
-let Enter: HTMLInputElement = (document.getElementById("inputText") as HTMLInputElement)
-let taskNumber: number = 0
+window.addEventListener("load", function(): void{ 
 
-Enter.addEventListener("keypress", function (event: KeyboardEvent): void { 
-    if (event.key == "enter") { 
-        addTask(); 
-        Enter.value = " "; 
+    let enter: HTMLInputElement = (document.getElementById("inputText") as HTMLInputElement); 
+    let tasksNumber: number = 0; 
+
+    enter.addEventListener("keypress", function (event: KeyboardEvent): void { 
+        if (event.key == "Enter") { 
+            addTask();
+            enter.value = " ";
+        }
+    });
+
+    function addTask(): void { 
+
+       const newDiv = document.createElement ("div");
+       newDiv.className ="myClass";
+
+        const Kreis: HTMLImageElement = document.createElement("img");
+        Kreis.className = "checkBox"; 
+        Kreis.setAttribute("src", "u5.svg");
+        Kreis.setAttribute("width", "40");
+        Kreis.setAttribute("height", "40");
+
+        const Text: HTMLLabelElement = document.createElement("label");
+        Text.innerHTML = enter.value; 
+        Text.className = "taskText";
+        Text.setAttribute("width", "600");
+        Text.setAttribute("height", "40");
+
+        const Haken: HTMLImageElement = document.createElement("img");
+        Haken.className = "Haken";
+        Haken.setAttribute("src", "u7.svg");
+        Haken.setAttribute("width", "20");
+
+        const trash: HTMLImageElement = document.createElement("img"); 
+        trash.setAttribute("src", "u8.svg");
+        trash.className = "trash"; 
+        trash.setAttribute("width", "30"); 
+        trash.setAttribute("height", "30")
+
+        document.getElementById("toDoList").appendChild(newDiv); 
+        newDiv.appendChild(Kreis); 
+        newDiv.appendChild(Text); 
+        newDiv.appendChild(trash); 
+    
+
+        Kreis.addEventListener("click", function(){ 
+            newDiv.appendChild(Haken);
+
+        })
+
+        Haken.addEventListener("click", function(){ 
+            newDiv.removeChild(Haken)
+        })
+
+        function deleteTask(div: HTMLDivElement): void { 
+            newDiv.remove(); 
+
+            tasksNumber--; 
+            tasksAnzahl(); 
+        };
+
+        trash.addEventListener("click", function (event: MouseEvent): void { 
+            console.log(event); 
+            deleteTask(newDiv); 
+        }); 
+
+        tasksNumber++; 
+        tasksAnzahl(); 
     }
+
+        function tasksAnzahl(): void { 
+            document.getElementById("counter").innerText = tasksNumber + " insgesamt"
+        };
+
+
+
 }); 
-
-function addTask(): void { 
-
-}
-
-const wrapper = document.querySelector(".wrapper")
-const newDiv = document.createElement("div")
-newDiv.className = "myClass"
-newDiv.id = "myId"
-newDiv.innerHTML = "essen"
-
-
-wrapper.appendChild(newDiv)
