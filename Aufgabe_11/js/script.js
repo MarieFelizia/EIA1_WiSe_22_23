034dde0e47d8242cf0119fc18d8ab964d074f1db
MarieFelizia <115719520+MarieFelizia@users.noreply.github.com>
2023-01-11 10:50:42 +0100

    Spracheingabe

--- Aufgabe_11/js/script.js
+++ Aufgabe_11/js/script.js
@@ -186,4 +186,34 @@ function deleteTodo(index) {
      */
     drawListToDOM();
 }
+//window.addEventListener("load", function(): void {
+const artyom = new Artyom();
+artyom.addCommands({
+    indexes: ["erstelle Aufgabe *"],
+    smart: true,
+    action: function (i, wildcard) {
+        console.log("Neue Aufgabe wird erstellt: " + wildcard);
+        let Eingabe = {
+            name: wildcard,
+            checked: false
+        };
+        Todo.unshift(Eingabe);
+        drawListToDOM();
+    }
+});
+function startContinuousArtyom() {
+    artyom.fatality();
+    setTimeout(function () {
+        artyom.initialize({
+            lang: "de-DE",
+            continuous: true,
+            listen: true,
+            interimResults: true,
+            debug: true
+        }).then(function () {
+            console.log("Ready!");
+        });
+    }, 250);
+}
+startContinuousArtyom();
 //# sourceMappingURL=script.js.map
